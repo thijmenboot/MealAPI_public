4f04c494a714134fd6a0120df738ec910d4834c9
thijmenboot <thijmenboot@outlook.com>
2018-01-21 18:18:44 +0100

    Revert "changed port"
    This reverts commit 2521d81517c59ed9562742ee700e903adb40511f.

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -6,7 +6,7 @@ const connectionSettings = {
     user: process.env.DB_USER || config.dbUser,
     password: process.env.DB_PASSWORD || config.dbPassword,
     database: process.env.DB_DATABASE || config.dbDatabase,
-    port: process.env.DB_PORT || 3306,
+    port: process.env.DB_PORT || config.dbPort,
     debug: false
 }
 const reconnectTimeout = 2000; // ms.
